7ee42033197cc18063ffc27f3397d673b406b48f
Akash Singh <singhakashthakuri07@gmail.com>
2026-05-15 20:47:19 +0930

    Update alterations.py

--- alterations.py
+++ alterations.py
@@ -1,4 +1,11 @@
 """
+GROUP DAN/EXT 32:
+
+•	ANGEL SHAHI         (s400420)
+•	ANJIT SHRESTHA      (s400533)
+•	AKASH SINGH         (s401643)
+•	DANIEL HARTREE      (s394765)
+
 alterations.py
 --------------
 All image-alteration logic for the game.
